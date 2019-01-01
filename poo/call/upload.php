6da0c1696cf746be665208ad2cc6ee7a1414416f
ERICANTOINE <antoine.micro@gmail.com>
2019-01-01 19:03:18 +0100

    finish upload

--- poo/call/upload.php
+++ poo/call/upload.php
@@ -3,10 +3,12 @@
     if(!empty($_FILES) && !empty($_FILES["image"]["name"]))
     {
         $verif = getimagesize($_FILES["image"]["tmp_name"]);
-        $path = "../../userImages/" . $_SESSION["id"] . "_" . $_SESSION["lastname"] . "_" . $_SESSION["firstname"] . "/";
+        $path = "../../userImages/" . $_SESSION["id"] . "_" . $_SESSION["lastname"] . "_" . $_SESSION["firstname"];
+        $tmp = $_FILES["image"]["tmp_name"];
         if($verif !== false)
         {
-            mkdir($path, 0755);
+            //mkdir($path);
+            move_uploaded_file($tmp, $path. $_FILES["image"]["name"]);
         }
         else
         {
